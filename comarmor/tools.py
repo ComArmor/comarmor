321283e967a1e5df5cc3befc3de8cd010496710a
ruffsl <roxfoxpox@gmail.com>
2017-10-16 17:36:18 -0700

    Clean Profile tests working parsing profiles brings a lot of bagage from apparmor though.

--- comarmor/tools.py
+++ comarmor/tools.py
@@ -1,5 +1,6 @@
 # ----------------------------------------------------------------------
 #    Copyright (C) 2013 Kshitij Gupta <kgupta8592@gmail.com>
+#    Copyright (C) 2017 Ruffin White <roxfoxpox@gmail.com>
 #
 #    This program is free software; you can redistribute it and/or
 #    modify it under the terms of version 2 of the GNU General Public
@@ -14,41 +15,39 @@
 import os
 import sys
 
-import apparmor.aa as apparmor
 import apparmor.ui as aaui
-from apparmor.common import user_perm, cmd
+import comarmor.ca as comarmor
+from comarmor.common import user_perm, cmd
 
 # setup module translations
 from apparmor.translations import init_translation
 _ = init_translation()
 
-class aa_tools:
+class ca_tools:
     def __init__(self, tool_name, args):
-        apparmor.init_aa()
 
         self.name = tool_name
-        self.profiledir = args.dir
+        self.profiledir = comarmor.get_full_path(args.dir)
         self.profiling = args.program
         self.check_profile_dir()
         self.silent = None
-        self.do_reload = args.do_reload
+        comarmor.init_ca(profiledir=self.profiledir)
 
         if tool_name in ['audit']:
             self.remove = args.remove
         elif tool_name == 'autodep':
             self.force = args.force
-            self.aa_mountpoint = apparmor.check_for_apparmor()
+            self.aa_mountpoint = comarmor.check_for_comarmor()
         elif tool_name == 'cleanprof':
             self.silent = args.silent
 
     def check_profile_dir(self):
         if self.profiledir:
-            apparmor.profile_dir = apparmor.get_full_path(self.profiledir)
-            if not os.path.isdir(apparmor.profile_dir):
-                raise apparmor.AppArmorException("%s is not a directory." % self.profiledir)
+            if not os.path.isdir(self.profiledir):
+                raise comarmor.ComArmorException("%s is not a directory." % self.profiledir)
 
-        if not user_perm(apparmor.profile_dir):
-            raise apparmor.AppArmorException("Cannot write to profile directory: %s" % (apparmor.profile_dir))
+        if not user_perm(self.profiledir):
+            raise comarmor.ComArmorException("Cannot write to profile directory: %s" % (self.profiledir))
 
     def get_next_to_profile(self):
         '''Iterator function to walk the list of arguments passed'''
@@ -59,47 +58,33 @@ class aa_tools:
 
             program = None
             profile = None
-            if os.path.exists(p) or p.startswith('/'):
-                fq_path = apparmor.get_full_path(p).strip()
-                if os.path.commonprefix([apparmor.profile_dir, fq_path]) == apparmor.profile_dir:
+            if p.startswith('/'):
+                fq_path = p.strip()
+                if os.path.commonprefix([comarmor.profile_dir, fq_path]) == comarmor.profile_dir:
                     program = None
                     profile = fq_path
                 else:
                     program = fq_path
-                    profile = apparmor.get_profile_filename(fq_path)
-            else:
-                which = apparmor.which(p)
-                if which is not None:
-                    program = apparmor.get_full_path(which)
-                    profile = apparmor.get_profile_filename(program)
-                elif os.path.exists(os.path.join(apparmor.profile_dir, p)):
-                    program = None
-                    profile = apparmor.get_full_path(os.path.join(apparmor.profile_dir, p)).strip()
-                else:
-                    if '/' not in p:
-                        aaui.UI_Info(_("Can't find %(program)s in the system path list. If the name of the application\nis correct, please run 'which %(program)s' as a user with correct PATH\nenvironment set up in order to find the fully-qualified path and\nuse the full path as parameter.") % { 'program': p })
-                    else:
-                        aaui.UI_Info(_("%s does not exist, please double-check the path.") % p)
-                    continue
+                    profile = comarmor.get_profile_filename(fq_path)
 
             yield (program, profile)
 
     def cleanprof_act(self):
         # used by aa-cleanprof
-        apparmor.read_profiles()
+        comarmor.read_profiles()
 
         for (program, profile) in self.get_next_to_profile():
             if program is None:
                 program = profile
 
-            if not program or not(os.path.exists(program) or apparmor.profile_exists(program)):
+            if not program or not(os.path.exists(program) or comarmor.profile_exists(program)):
                 if program and not program.startswith('/'):
                     program = aaui.UI_GetString(_('The given program cannot be found, please try with the fully qualified path name of the program: '), '')
                 else:
                     aaui.UI_Info(_("%s does not exist, please double-check the path.") % program)
                     sys.exit(1)
 
-            if program and apparmor.profile_exists(program):
+            if program and comarmor.profile_exists(program):
                 self.clean_profile(program)
 
             else:
@@ -110,101 +95,44 @@ class aa_tools:
                     sys.exit(1)
 
     def cmd_disable(self):
-        apparmor.read_profiles()
+        comarmor.read_profiles()
 
         for (program, profile) in self.get_next_to_profile():
 
             output_name = profile if program is None else program
 
-            if not os.path.isfile(profile) or apparmor.is_skippable_file(profile):
+            if not os.path.isfile(profile) or comarmor.is_skippable_file(profile):
                 aaui.UI_Info(_('Profile for %s not found, skipping') % output_name)
                 continue
 
             aaui.UI_Info(_('Disabling %s.') % output_name)
             self.disable_profile(profile)
 
-            self.unload_profile(profile)
-
-    def cmd_enforce(self):
-        apparmor.read_profiles()
-
-        for (program, profile) in self.get_next_to_profile():
-
-            output_name = profile if program is None else program
-
-            if not os.path.isfile(profile) or apparmor.is_skippable_file(profile):
-                aaui.UI_Info(_('Profile for %s not found, skipping') % output_name)
-                continue
-
-            apparmor.set_enforce(profile, program)
-
-            self.reload_profile(profile)
-
-    def cmd_complain(self):
-        apparmor.read_profiles()
-
-        for (program, profile) in self.get_next_to_profile():
-
-            output_name = profile if program is None else program
-
-            if not os.path.isfile(profile) or apparmor.is_skippable_file(profile):
-                aaui.UI_Info(_('Profile for %s not found, skipping') % output_name)
-                continue
-
-            apparmor.set_complain(profile, program)
-
-            self.reload_profile(profile)
-
-    def cmd_audit(self):
-        apparmor.read_profiles()
-
-        for (program, profile) in self.get_next_to_profile():
-
-            output_name = profile if program is None else program
-
-            if not os.path.isfile(profile) or apparmor.is_skippable_file(profile):
-                aaui.UI_Info(_('Profile for %s not found, skipping') % output_name)
-                continue
-
-            # keep this to allow toggling 'audit' flags
-            if not self.remove:
-                aaui.UI_Info(_('Setting %s to audit mode.') % output_name)
-            else:
-                aaui.UI_Info(_('Removing audit mode from %s.') % output_name)
-            apparmor.change_profile_flags(profile, program, 'audit', not self.remove)
-
-            disable_link = '%s/disable/%s' % (apparmor.profile_dir, os.path.basename(profile))
-
-            if os.path.exists(disable_link):
-                aaui.UI_Info(_('\nWarning: the profile %s is disabled. Use aa-enforce or aa-complain to enable it.') % os.path.basename(profile))
-
-            self.reload_profile(profile)
-
     def cmd_autodep(self):
-        apparmor.read_profiles()
+        comarmor.read_profiles()
 
         for (program, profile) in self.get_next_to_profile():
             if not program:
                 aaui.UI_Info(_('Please pass an application to generate a profile for, not a profile itself - skipping %s.') % profile)
                 continue
 
-            apparmor.check_qualifiers(program)
+            comarmor.check_qualifiers(program)
 
-            if os.path.exists(apparmor.get_profile_filename(program)) and not self.force:
+            if os.path.exists(comarmor.get_profile_filename(program)) and not self.force:
                 aaui.UI_Info(_('Profile for %s already exists - skipping.') % program)
             else:
-                apparmor.autodep(program)
+                comarmor.autodep(program)
                 if self.aa_mountpoint:
-                    apparmor.reload(program)
+                    comarmor.reload(program)
 
     def clean_profile(self, program):
-        filename = apparmor.get_profile_filename(program)
-        import apparmor.cleanprofile as cleanprofile
+        filename = comarmor.get_profile_filename(program)
+        import comarmor.cleanprofile as cleanprofile
         prof = cleanprofile.Prof(filename)
         cleanprof = cleanprofile.CleanProf(True, prof, prof)
         deleted = cleanprof.remove_duplicate_rules(program)
         aaui.UI_Info(_("\nDeleted %s rules.") % deleted)
-        apparmor.changed[program] = True
+        comarmor.changed[program] = True
 
         if filename:
             if not self.silent:
@@ -220,39 +148,18 @@ class aa_tools:
                 while ans != 'CMD_SAVE_CHANGES':
                     ans, arg = q.promptUser()
                     if ans == 'CMD_SAVE_CHANGES':
-                        apparmor.write_profile_ui_feedback(program)
-                        self.reload_profile(filename)
+                        comarmor.write_profile_ui_feedback(program)
                     elif ans == 'CMD_VIEW_CHANGES':
-                        #oldprofile = apparmor.serialize_profile(apparmor.original_aa[program], program, '')
-                        newprofile = apparmor.serialize_profile(apparmor.aa[program], program, '')
-                        apparmor.display_changes_with_comments(filename, newprofile)
+                        #oldprofile = comarmor.serialize_profile(comarmor.original_aa[program], program, '')
+                        newprofile = comarmor.serialize_profile(comarmor.aa[program], program, '')
+                        comarmor.display_changes_with_comments(filename, newprofile)
             else:
-                apparmor.write_profile_ui_feedback(program)
-                self.reload_profile(filename)
+                comarmor.write_profile_ui_feedback(program)
         else:
-            raise apparmor.AppArmorException(_('The profile for %s does not exists. Nothing to clean.') % program)
+            raise comarmor.AppArmorException(_('The profile for %s does not exists. Nothing to clean.') % program)
 
     def enable_profile(self, filename):
-        apparmor.delete_symlink('disable', filename)
+        comarmor.delete_symlink('disable', filename)
 
     def disable_profile(self, filename):
-        apparmor.create_symlink('disable', filename)
-
-    def unload_profile(self, profile):
-        if not self.do_reload:
-            return
-
-        # FIXME: should ensure profile is loaded before unloading
-        cmd_info = cmd([apparmor.parser, '-I%s' % apparmor.profile_dir, '--base', apparmor.profile_dir, '-R', profile])
-
-        if cmd_info[0] != 0:
-            raise apparmor.AppArmorException(cmd_info[1])
-
-    def reload_profile(self, profile):
-        if not self.do_reload:
-            return
-
-        cmd_info = cmd([apparmor.parser, '-I%s' % apparmor.profile_dir, '--base', apparmor.profile_dir, '-r', profile])
-
-        if cmd_info[0] != 0:
-            raise apparmor.AppArmorException(cmd_info[1])
+        comarmor.create_symlink('disable', filename)
