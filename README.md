3b809c3ca0c247266842e2b68245e4cac3df787b
ruffsl <roxfoxpox@gmail.com>
2018-02-12 23:28:16 -0800

    Adding boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # comarmor
-Like AppArmor, but for Secure Communications
+
+[![pypi](https://img.shields.io/pypi/v/comarmor.svg?branch=master)](https://pypi.python.org/pypi/comarmor/)
+[![docs](https://readthedocs.org/projects/comarmor/badge/?version=latest)](https://readthedocs.org/projects/comarmor)
+[![build](https://travis-ci.org/comarmor/comarmor.svg?branch=master)](https://travis-ci.org/comarmor/comarmor)
+[![codecov](https://codecov.io/github/comarmor/comarmor/coverage.svg?branch=master)](https://codecov.io/github/comarmor/comarmor?branch=master)
+
+Interpreter library for comarmor policy profiles
--- setup.py
+++ setup.py
@@ -1,13 +1,32 @@
-from distutils.core import setup
+from setuptools import find_packages
+from setuptools import setup
 
 setup(
     name='comarmor',
     version='0.0.0',
-    packages=['comarmor'],
-    url='https://github.com/ComArmor/comarmor',
-    download_url = 'https://github.com/ComArmor/comarmor/archive/0.0.0.tar.gz',
-    license='',
-    author='',
-    author_email='',
-    description='Like AppArmor, but for Secure Communications'
+    packages=find_packages(exclude=['test']),
+    author='Ruffin White',
+    author_email='ruffin@osrfoundation.org',
+    maintainer='Ruffin White',
+    maintainer_email='ruffin@osrfoundation.org',
+    url='https://github.com/comarmor/comarmor/wiki',
+    download_url='https://github.com/comarmor/comarmor/releases',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='ComArmor is like AppArmor, but for Secure Communications.',
+    long_description="""\
+ComArmor is a profile interpreter library for policy files,
+and provides tooling to create and process access control policies.""",
+    license='Apache License, Version 2.0',
+    test_suite='test',
+    package_data={
+        'comarmor': [
+            'schema/profile/*',
+        ],
+    },
 )
